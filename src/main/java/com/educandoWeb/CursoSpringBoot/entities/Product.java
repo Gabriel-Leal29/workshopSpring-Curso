28c2a037f7a78d1006027a3e2a4a805d22eb5f59
Gabriel Leal <gabriellealdeoliveira2005@gmail.com>
2025-08-03 18:50:20 -0300

    Adicionado a lógica de 1 pra muitos do produto e ordem item

--- src/main/java/com/educandoWeb/CursoSpringBoot/entities/Product.java
+++ src/main/java/com/educandoWeb/CursoSpringBoot/entities/Product.java
@@ -1,5 +1,6 @@
 package com.educandoWeb.CursoSpringBoot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -31,7 +32,8 @@ public class Product implements Serializable {
     //primeiro o nome da key dessa classe, depois da classe inversa, no caso a da categoria
     private Set<Category> categories = new HashSet<>();
 
-
+    @OneToMany(mappedBy="id.product")//1 para mts
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {};
 
@@ -86,6 +88,15 @@ public class Product implements Serializable {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
+    };
 
     @Override
     public boolean equals(Object o) {
